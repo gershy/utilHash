b33e99367494a59e46ebea456fa0e79fb5ad8a96
Gershom Maes <gershommaes@protonmail.com>
2026-03-08 22:02:21 -0400

    update deps

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,8 @@ import { isCls } from '@gershy/clearing';
 export default async (data: string | Uint8Array, te = new TextEncoder()) => {
   
   const result = await crypto.subtle.digest('sha-256', isCls(data, String) ? te.encode(data) : data);
-  
-  const arr: number[] = result.byteLength[toArr](n => result[n]);
-  
-  console.log({ result, arr });
-  
-  return [ ...result ];
+  return result
+    [toNum]()
+    [toStr](String[base62]);
   
 };
